bbfab24e82aff52d0d90c9c69bc6eb006028065d
sarcasticDweller <47394104+sarcasticDweller@users.noreply.github.com>
2025-12-12 15:16:19 +0000

    Added compiler script and setup script

--- test.py
+++
@@ -1,4 +0,0 @@
-import easyttuimenus as ttui # pyright: ignore[reportMissingTypeStubs]
-options = ["cherry", "apple", "banana"]
-picked = ttui.multiple_choice_menu("Here's some fruit", options)
-print(picked)
