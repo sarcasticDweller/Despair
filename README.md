4fa36c1aa6f3de27ab5a8820a79de8761b392e0b
sarcasticDweller <47394104+sarcasticDweller@users.noreply.github.com>
2025-12-16 19:34:27 +0000

    Hackin even further away from the target

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ A rummy-inspired card game about debt, consequences, and information.
 
 # Dev Diary
 
+## 2025-12-16
+
+### 02:33 P.M.
+
+That's it, I've had it! I try to learn PyGame, and I'm defeated by my own incompetence.
+
+I try to learn unit testing to help validate my work in an environment that lacks an X server, and I'm defeated there!
+
+I quit, I'm putting this down for today. 
+
 ## 2025-12-12
 
 ### 11:51 A.M.
--- tests/test_cards_with_pygame.py
+++ tests/test_cards_with_pygame.py
@@ -1,8 +1,11 @@
-import cards_with_pygame as cwp
+#import cards_with_graphics as cwp
 import playing_cards as pc
 import unittest
 
 class TestCardSpriteInitialization(unittest.TestCase):
+    def test_unit_tests_work(self):
+        card = pc.Card(pc.Suit.HEART, pc.Rank.ACE)
+        self.assertEqual(card.suit, pc.Suit.HEART)
     def test_card_sprite_creation(self):
         card_data = pc.Card(pc.Suit.HEART, pc.Rank.ACE)
         card = cwp.CardSprite(card_data, facing_up=False)
