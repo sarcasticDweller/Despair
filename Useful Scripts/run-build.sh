393cb4486cd7a06578e3fa498d807d0ea597af54
sarcasticDweller <capslocked.strife@gmail.com>
2025-12-22 10:53:37 -0500

    Removed redundant script

--- Useful Scripts/run-build.sh
+++ Useful Scripts/run-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# add me to path: `export PATH=$PATH:Useful\ Scripts/run-build.sh`
 source venv/bin/activate
+pip install --upgrade pip
 pip install -r src/requirements.txt
 python3 setup.py build
 ./build/exe.macosx-15.0-arm64-3.13/despair
