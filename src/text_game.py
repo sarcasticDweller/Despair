e98830e1a8799f5c4da0a22f6aee956d3eb3d4e1
sarcasticDweller <capslocked.strife@gmail.com>
2025-12-27 20:03:37 -0500

    Fixed bad logic

--- src/text_game.py
+++ src/text_game.py
@@ -13,17 +13,6 @@ def game_loop() -> None:
     deck.draw(int(len(deck)*STARTING_DECK_FRACTION)) # starting out with half a deck of cards. ooh, the uncertainty!
     hand = playing_cards.Deck(*deck.draw(HAND_SIZE))
 
-    def validate_and_remove(valid: bool, card_nest: list[list[playing_cards.Card]]) -> bool:
-        """
-        Returns `valid` parameter as a way to check if the function executed successfully. If so, removes the played cards from `considered_cards` and adds them to `played_cards`.
-        """
-        if valid:
-            for cards in card_nest:
-                played_cards.extend(cards)
-                for card in cards:
-                    considered_cards.remove(card)
-        return valid
-
     print("Despair: A Game About Debt")
     pause()
     print("How to Play:\n- Select cards by typing the number left of the colon and press enter\n- Select pairs to play pairs\n- Select sequences of three of the same suit to play straights\n- All other cards will be discarded\n- Press \"0\" when you've selected what you want to play to end your turn\n- When you run out of cards in the deck you get one last turn to play cards\n- All discarded cards and cards left in your hand will weigh against you!")
@@ -38,15 +27,18 @@ def game_loop() -> None:
         considered_cards.extend(hand.draw_several_specific(selected))
         contains_pairs, pairs = considered_cards.contains_pairs()
         contains_straights, straights = considered_cards.contains_straights()
-        
-        
-        if validate_and_remove(contains_pairs, pairs):
-            print("You played pairs!")
-        elif validate_and_remove(contains_straights, straights):
-            print("You played straights!")
+        valid = contains_pairs or contains_straights
+
+        if valid:
+            played_cards.extend(*pairs, *straights)
+            considered_cards.clear()
         else:
             print("I hope you wanted to discard those!")
             discard.extend(considered_cards.empty_deck())
+        if contains_pairs:
+            print("You played pairs!")
+        if contains_straights:
+            print("You played straights!")
         pause()
 
     discard.extend(hand.empty_deck())
