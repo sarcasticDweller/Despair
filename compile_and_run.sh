f61a72e6eeb601ae85b0ba254d27a46a4e660e85
sarcasticDweller <capslocked.strife@gmail.com>
2025-12-28 13:30:16 -0500

    Make compile_and_run.sh more dynamic

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [! -d "venv" ]; then
+    python3 -m venv venv/
+    echo "No venv/ folder found, creating venv"
+else
+    echo "venv/ folder found"
+fi
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r src/requirements.txt
