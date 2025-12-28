02b32d1285de68e91fd4a88b2b42064c60db79c9
sarcasticDweller <capslocked.strife@gmail.com>
2025-12-27 19:15:17 -0500

    Decks should be fancy lists

--- src/playing_cards.py
+++ src/playing_cards.py
@@ -1,4 +1,5 @@
 import random, enum
+from typing import List
 
 class Suit(enum.Enum):
     SPADE = "spade"
@@ -45,65 +46,43 @@ class Card:
         #return self.rank > other.rank
         return True if self.rank == Rank.ACE and other.rank == Rank.KING else self.rank > other.rank
     
-class Deck:
-    def __init__(self, cards: list[Card] = []) -> None:
-        self.cards = cards
+class Deck(List[Card]):
+    def __init__(self, *cards: Card) -> None:
+        super().__init__(cards)
 
-    def __repr__(self) -> str: 
-        cards = ""
-        for card in self.cards:
-            cards += f", {card}"
-        return cards
-    
-    def get_list_of_cards_as_strings(self) -> list[str]: # this *heavily* resembles __repr__... should these be merged?
-        card_strings: list[str] = []
-        for card in self.cards:
-            card_strings.append(f"{card}")
-        return card_strings
+    def get_list_of_cards_as_strings(self) -> List[str]: # this *heavily* resembles __repr__... should these be merged?
+        return [f"{card}" for card in self]
 
     def shuffle(self) -> None:
-        random.shuffle(self.cards)
+        random.shuffle(self)
 
-    def draw(self, count: int = 1) -> list[Card]:
+    def draw(self, count: int = 1) -> List[Card]:
         """Will draw `count` cards from the top of the deck and return them as a list of `Card` objects. Will error if there are not enough cards in the deck."""
-        drawn_cards, self.cards = self.cards[:count], self.cards[count:]
-        return drawn_cards
+        return [self.pop() for _ in range(count)]
 
-    def draw_specific_by_index(self, card_index: int) -> Card:
-        return self.cards.pop(card_index)
-    
-    def remove_specific(self, card: Card) -> None:
-        self.cards.remove(card)
-
-    def draw_several_specific(self, card_indexes: list[int]) -> list[Card]:
-        drawn_cards: list[Card] = []
+    def draw_several_specific(self, card_indexes: List[int]) -> List[Card]:
+        drawn_cards: List[Card] = []
         for i in card_indexes:
-            drawn_cards.append(self.cards[i - 1])
-        self.cards = list(set(self.cards).symmetric_difference(drawn_cards)) # removes drawn cards from deck
+            drawn_cards.append(self[i - 1])
+        self.cards = list(set(self).symmetric_difference(drawn_cards)) # removes drawn cards from deck
         return drawn_cards
     
-    def empty_deck(self) -> list[Card]:
-        cards = self.cards
-        self.cards = []
-        return cards
+    def empty_deck(self) -> List[Card]:
+        old_cards = list(self)
+        self.clear()
+        return old_cards
 
-    def add(self, card: Card) -> None:
-        self.cards.append(card)
-
-    def add_several_to_top_of_deck(self, cards: list[Card]) -> None:
-        self.cards = self.cards + cards
-
-    def get_card_data_as_lists(self) -> tuple[list[Suit], list[Rank]]: 
-        suits: list[Suit] = []
-        ranks: list[Rank] = []
-        for card in self.cards:
+    def get_card_data_as_lists(self) -> tuple[List[Suit], List[Rank]]: 
+        suits: List[Suit] = []
+        ranks: List[Rank] = []
+        for card in self:
             suits.append(card.suit)
             ranks.append(card.rank)
         return suits, ranks
 
     def is_straight(self, length: int = 3) -> bool:
         """Depricated in favor of `contains_straights()`... ?"""
-        if len(self.cards) != length:
+        if len(self) != length:
             return False
         suits, ranks = self.get_card_data_as_lists()
         if len(set(suits)) != 1: # eliminate duplicates and count uniques, of which there should only be one
@@ -116,23 +95,23 @@ class Deck:
     
     def is_pair(self, length: int = 2) -> bool:
         """Depricated in favor of `contains_pairs()`... ?"""
-        if len(self.cards) != length:
+        if len(self) != length:
             return False
         _, ranks = self.get_card_data_as_lists()
         return len(set(ranks)) == 1
     
-    def contains_pairs(self, size_of_pair: int = 2) -> tuple[bool, list[list[Card]]]:
+    def contains_pairs(self, size_of_pair: int = 2) -> tuple[bool, List[List[Card]]]:
         """Checks the deck for pairs of a given length. Supports two-of-a-kind, three-of-a-kind, etc.
         Args:
             size_of_pair (int, optional): The length of the pairs to check for. Defaults to 2 to search for standard pairs.
         Returns:
-            tuple[bool, list[list[Card]]]:
+            tuple[bool, List[List[Card]]]:
                 - A boolean indicating whether any pairs of the specified length were found.
                 - A list of lists, where each inner list contains `Card` objects representing a pair.
         """
-        ranks_dict: dict[int, list[Card]] = {}
-        pairs: list[list[Card]] = []
-        for card in self.cards:
+        ranks_dict: dict[int, List[Card]] = {}
+        pairs: List[List[Card]] = []
+        for card in self:
             if card.rank not in ranks_dict:
                 ranks_dict[card.rank] = []
             if card not in ranks_dict[card.rank]:
@@ -159,7 +138,7 @@ class Deck:
 
         return len(pairs) > 0, pairs
 
-    def contains_straights(self, size_of_straight: int = 3) -> tuple[bool, list[list[Card]]]:
+    def contains_straights(self, size_of_straight: int = 3) -> tuple[bool, List[List[Card]]]:
         """Checks the deck for straights of a given length. Supports three-card straights, four-card straights, etc. Only supports low aces... This method needs to be further fleshed-out.
 
         :param size_of_straight: The length of the straights to check for. Defaults to 3 to search for standard straights.
@@ -167,14 +146,14 @@ class Deck:
         :return: A tuple containing:
             - A boolean indicating whether any straights of the specified length were found.
             - A list of lists, where each inner list contains `Card` objects representing a straight
-        :rtype: tuple[bool, list[list[Card]]]
+        :rtype: tuple[bool, List[List[Card]]]
         """
 
-        suits_dict: dict[Suit, list[Card]] = {}
-        straights: list[list[Card]] = []
+        suits_dict: dict[Suit, List[Card]] = {}
+        straights: List[List[Card]] = []
 
         # group by suit
-        for card in self.cards:
+        for card in self:
             if card.suit not in suits_dict:
                 suits_dict[card.suit] = []
             suits_dict[card.suit].append(card) # always add the card to respect larger card decks
@@ -189,9 +168,9 @@ class Deck:
 
             # convert cards to list of ints for simpler analysis
             card_ranks = [int(card.rank) for card in suited_cards]
-            found_sequences: list[list[int]] = find_unique_sequences(card_ranks, size_of_straight)
+            found_sequences: List[List[int]] = find_unique_sequences(card_ranks, size_of_straight)
 
-            # convert found_sequences back into list[list[card]]
+            # convert found_sequences back into List[List[card]]
             for sequence in found_sequences:
                 straight_cards = [card for rank in sequence for card in suited_cards if card.rank == rank]
                 straights.append(straight_cards)
@@ -201,24 +180,23 @@ class Deck:
     
 class StandardDeck(Deck):
     def __init__(self) -> None:
-        super().__init__(_standard_deck_generator())
+        super().__init__(*_standard_deck_generator())
 
-def find_unique_sequences(master_sequence: list[int], sequence_length: int) -> list[list[int]]:
-    """Takes a list of integers and returns a two-dimensional list of consecutive sequences of `sequence_length` length found in the list of integers."""
-    ms_in_order = sorted(master_sequence)
-    working_sequence: list[int] = []
-    sequences: list[list[int]] = []
+def find_unique_sequences(master_sequence: List[int], sequence_length: int) -> List[List[int]]:
+    """Takes a list of integers and returns a two-dimensional list of consecutive sequences of `sequence_length` length found in the list of integers. Expects the list to be sorted."""
+    working_sequence: List[int] = []
+    sequences: List[List[int]] = []
     for i in range(len(master_sequence)):
         if len(working_sequence) == 0:
-            working_sequence.append(ms_in_order[i])
+            working_sequence.append(master_sequence[i])
             continue
-        if ms_in_order[i] - 1 == working_sequence[-1]:
-            working_sequence.append(ms_in_order[i])
+        if master_sequence[i] - 1 == working_sequence[-1]:
+            working_sequence.append(master_sequence[i])
         if len(working_sequence) == sequence_length:
             sequences.append(working_sequence)
             working_sequence = []
 
     return sequences
 
-def _standard_deck_generator() -> list[Card]:
+def _standard_deck_generator() -> List[Card]:
     return [Card(s, r) for s in Suit for r in Rank]
--- src/text_game.py
+++ src/text_game.py
@@ -10,32 +10,35 @@ def game_loop() -> None:
     considered_cards = playing_cards.Deck()
     deck = playing_cards.StandardDeck()
     deck.shuffle()
-    deck.draw(int(len(deck.cards)*STARTING_DECK_FRACTION)) # starting out with half a deck of cards. ooh, the uncertainty!
-    hand = playing_cards.Deck(deck.draw(HAND_SIZE))
+    deck.draw(int(len(deck)*STARTING_DECK_FRACTION)) # starting out with half a deck of cards. ooh, the uncertainty!
+    hand = playing_cards.Deck(*deck.draw(HAND_SIZE))
+
+    def validate_and_remove(valid: bool, card_nest: list[list[playing_cards.Card]]) -> bool:
+        """
+        Returns `valid` parameter as a way to check if the function executed successfully. If so, removes the played cards from `considered_cards` and adds them to `played_cards`.
+        """
+        if valid:
+            for cards in card_nest:
+                played_cards.extend(cards)
+                for card in cards:
+                    considered_cards.remove(card)
+        return valid
 
     print("Despair: A Game About Debt")
     pause()
     print("How to Play:\n- Select cards by typing the number left of the colon and press enter\n- Select pairs to play pairs\n- Select sequences of three of the same suit to play straights\n- All other cards will be discarded\n- Press \"0\" when you've selected what you want to play to end your turn\n- When you run out of cards in the deck you get one last turn to play cards\n- All discarded cards and cards left in your hand will weigh against you!")
     pause()
-    while len(deck.cards) > 0:
+    while len(deck) > 0:
         try:
-            hand.add_several_to_top_of_deck(deck.draw(HAND_SIZE-len(hand.cards)))
+            hand.extend(deck.draw(HAND_SIZE-len(hand)))
         except:
-            hand.add_several_to_top_of_deck(deck.empty_deck())
+            hand.extend(deck.empty_deck())
         
-        selected = ttui.multiple_choice_menu(f"Despair\n\nStats\nDiscard: {len(discard.cards)}\nPlayed: {len(played_cards.cards)}\nRemaining cards in deck: {len(deck.cards)}\n\nSelect some cards to play", hand.get_list_of_cards_as_strings())
-        considered_cards.add_several_to_top_of_deck(hand.draw_several_specific(selected))
+        selected = ttui.multiple_choice_menu(f"Despair\n\nStats\nDiscard: {len(discard)}\nPlayed: {len(played_cards)}\nRemaining cards in deck: {len(deck)}\n\nSelect some cards to play", hand.get_list_of_cards_as_strings())
+        considered_cards.extend(hand.draw_several_specific(selected))
         contains_pairs, pairs = considered_cards.contains_pairs()
         contains_straights, straights = considered_cards.contains_straights()
         
-        def validate_and_remove(valid: bool, card_nest: list[list[playing_cards.Card]]) -> bool:
-            """Returns `valid` parameter as a way to check if the function executed successfully. If so, removes the played cards from `considered_cards` and adds them to `played_cards`."""
-            if valid:
-                for cards in card_nest:
-                    played_cards.add_several_to_top_of_deck(cards)
-                    for card in cards:
-                        considered_cards.remove_specific(card)
-            return valid
         
         if validate_and_remove(contains_pairs, pairs):
             print("You played pairs!")
@@ -43,12 +46,12 @@ def game_loop() -> None:
             print("You played straights!")
         else:
             print("I hope you wanted to discard those!")
-            discard.add_several_to_top_of_deck(considered_cards.empty_deck())
+            discard.extend(considered_cards.empty_deck())
         pause()
 
-    discard.add_several_to_top_of_deck(hand.empty_deck())
-    score_discard = len(discard.cards)
-    score_played = len(played_cards.cards)
+    discard.extend(hand.empty_deck())
+    score_discard = len(discard)
+    score_played = len(played_cards)
     score = score_played - score_discard
     result_message = "Good job!" if score > 0 else "Better luck next time!"
     print("And that's the game!\nYour remaining cards have been discarded.")
@@ -56,6 +59,7 @@ def game_loop() -> None:
     print("Thanks for playing!")
     pause()
     return
+
 
 if __name__ == "__main__":
     game_loop()
